e3dbcdc885b635bc4e33a6c8c2d451ff59d78b51
wjdrnrdl97 <vkvk1069@gmail.com>
2023-08-23 12:51:05 +0900

    checkout commit

--- src/main/java/backend/shop/com/multiplexshop/domain/board/entity/Board.java
+++ src/main/java/backend/shop/com/multiplexshop/domain/board/entity/Board.java
@@ -1,8 +1,26 @@
 package backend.shop.com.multiplexshop.domain.board.entity;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-public class Board {
-}
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
+    public class Board {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long boardId;
+
+        @Column(length = 30,nullable=false)
+        private String boardTitle;
+
+        @Column(length = 500,nullable=false)
+        private String boardContent;
+
+        @
+
+    }
