be7e6c60455c3a6bcde6788f6dcadbe65f74145b
wjdrnrdl97 <vkvk1069@gmail.com>
2023-08-23 13:52:13 +0900

    temporary

--- src/main/java/backend/shop/com/multiplexshop/domain/board/dto/BoardDTOs.java
+++ src/main/java/backend/shop/com/multiplexshop/domain/board/dto/BoardDTOs.java
@@ -1,4 +1,17 @@
 package backend.shop.com.multiplexshop.domain.board.dto;
 
+import jakarta.persistence.Access;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
 public class BoardDTOs {
+    @Getter
+    @NoArgsConstructor(Access=AccessLevel.PROTECTED)
+    @Builder
+    public static class BoardResponseDTO(){
+        private String boardTitle;
+    }
 }
