74faf96c914de68da0f6d0732152c3d102622225
kwon93 <133971731+kwon93@users.noreply.github.com>
2023-08-24 12:27:17 +0900

    Merge pull request #10 from kwon93/feat_Member_join
    feat:member entity temporary  create

--- src/main/java/backend/shop/com/multiplexshop/domain/member/entity/Member.java
+++ src/main/java/backend/shop/com/multiplexshop/domain/member/entity/Member.java
@@ -1,22 +1,40 @@
 package backend.shop.com.multiplexshop.domain.member.entity;
 
-import backend.shop.com.multiplexshop.domain.board.entity.Board;
+import backend.shop.com.multiplexshop.domain.common.BaseEntity;
 import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Builder
-public class Member {
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long memberId;
 
-    @OneToMany(mappedBy = "member",cascade = CascadeType.PERSIST)
-    private List<Board> boards = new ArrayList<>();
+    @Column(length = 30, nullable = false)
+    private String memberEmailId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(length = 20, nullable = false)
+    private String memberName;
+
+    @Column(length = 100)
+    private String memberAddress;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private Role role;
+
 }
