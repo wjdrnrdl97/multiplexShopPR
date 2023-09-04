e8b663a6dfebb2a4fbdd8f352a199a8af2347e19
kwon93 <93umsongaji@gmail.com>
2023-09-04 20:26:52 +0900

    feat: product 및 order_product Entity created

--- src/main/java/backend/shop/com/multiplexshop/domain/Products/entity/Products.java
+++ src/main/java/backend/shop/com/multiplexshop/domain/Products/entity/Products.java
@@ -1,4 +1,68 @@
 package backend.shop.com.multiplexshop.domain.Products.entity;
 
-public class Products {
+
+/**
+ * 셀렉트 태그 최종 컬럼명은 추후에 반영 예정.
+ */
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Products{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(length = 30, nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer productPrice;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+
+    @Column(length = 20)
+    private String selectTag1;
+
+    @Column(length = 20)
+    private String selectTag2;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10, nullable = false)
+    private Categories categories;
+
+    @Column(length = 100)
+    private String imagePath;
+
+    @Column(length = 100)
+    private String detailImagePath;
+
+    @Column(length = 200)
+    private String productScript;
+
+    @Builder
+    public Products(String productName,
+                    Integer productPrice,
+                    Integer stockQuantity,
+                    String selectTag1,
+                    String selectTag2,
+                    Categories categories,
+                    String imagePath,
+                    String detailImagePath,
+                    String productScript) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.stockQuantity = stockQuantity;
+        this.selectTag1 = selectTag1;
+        this.selectTag2 = selectTag2;
+        this.categories = categories;
+        this.imagePath = imagePath;
+        this.detailImagePath = detailImagePath;
+        this.productScript = productScript;
+    }
 }
--- src/main/java/backend/shop/com/multiplexshop/domain/orders/OrderProducts.java
+++ src/main/java/backend/shop/com/multiplexshop/domain/orders/OrderProducts.java
@@ -1,6 +1,51 @@
 package backend.shop.com.multiplexshop.domain.orders;
 
-import jakarta.persistence.Entity;
+import backend.shop.com.multiplexshop.domain.Products.entity.Products;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderProducts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_product_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    @Column(nullable = false)
+    private Long orderId;
+
+    @ManyToOne
+    @Column(nullable = false)
+    @JoinColumn(name = "product_id")
+    private Long productId;
+
+    @ManyToOne
+    @Column(nullable = false)
+    @JoinColumn(name = "member_id")
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Integer orderPrice;
+
+    @Column(nullable = false)
+    private Integer order_count;
+
+    @Builder
+    public OrderProducts(Long orderId, Long productId, Long memberId, Integer orderPrice, Integer order_count) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.memberId = memberId;
+        this.orderPrice = orderPrice;
+        this.order_count = order_count;
+    }
+
+
 }
