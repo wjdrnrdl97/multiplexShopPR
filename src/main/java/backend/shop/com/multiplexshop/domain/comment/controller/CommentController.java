68d08f184986237e35225f71fe67b095ad158cb4
kwon93 <93umsongaji@gmail.com>
2023-09-09 22:13:08 +0900

    feat: Comment PUT Requesr, DELETE Request HTTP StatusCode fix(200 OK -> 204 NoContent);

--- src/main/java/backend/shop/com/multiplexshop/domain/comment/controller/CommentController.java
+++ src/main/java/backend/shop/com/multiplexshop/domain/comment/controller/CommentController.java
@@ -3,10 +3,12 @@ package backend.shop.com.multiplexshop.domain.comment.controller;
 import backend.shop.com.multiplexshop.domain.comment.entity.Comment;
 import backend.shop.com.multiplexshop.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,7 @@ import static backend.shop.com.multiplexshop.domain.comment.dto.CommentDTOs.*;
 //@RestController
 @RequiredArgsConstructor
 @Controller
+@Slf4j
 public class CommentController {
 
     private final CommentService commentService;
@@ -49,11 +52,12 @@ public class CommentController {
     }
 
     @ResponseBody
-    @PutMapping(value = "/api/comment/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CommentResponseDTO> updateComment(@PathVariable("id") Long commentId,
-                                                 @RequestBody CommentRequestDTO commentRequestDTO){
-        CommentResponseDTO updatedComment = commentService.update(commentId, commentRequestDTO);
-        return ResponseEntity.ok().body(updatedComment);
+    @PutMapping(value = "/api/comment/{id}")
+    public ResponseEntity updateComment(@PathVariable("id") Long commentId,
+                                        @RequestBody CommentRequestDTO commentRequestDTO){
+        log.info("PUT REQUEST = {}", commentRequestDTO);
+        commentService.update(commentId, commentRequestDTO);
+        return ResponseEntity.noContent().build();
     }
 
     //수정 요청 뷰 메서드
@@ -70,6 +74,6 @@ public class CommentController {
     @DeleteMapping("/api/comment/{id}")
     public ResponseEntity deleteComment(@PathVariable("id")Long commentId) {
           commentService.deleteCommentById(commentId);
-          return ResponseEntity.ok().build();
+          return ResponseEntity.noContent().build();
     }
 }
