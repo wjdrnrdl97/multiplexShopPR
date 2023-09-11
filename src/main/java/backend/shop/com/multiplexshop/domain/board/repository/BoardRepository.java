7043d5c0e66707a989db39df6febd55c995e33bb
kdh93 <93umsongaji@gmail.com>
2023-09-11 12:52:11 +0900

    setting:  dev branch merge 예정

--- src/main/java/backend/shop/com/multiplexshop/domain/board/repository/BoardRepository.java
+++ src/main/java/backend/shop/com/multiplexshop/domain/board/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package backend.shop.com.multiplexshop.domain.board.repository;
 
 import backend.shop.com.multiplexshop.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+
+public interface BoardRepository{
 }
