ef200943236db4e06f0dba6061e36e7b30648224
wjdrnrdl97 <vkvk1069@gmail.com>
2023-09-19 12:08:55 +0900

    feat: orderAPIController postMapping 진행중

--- src/main/java/backend/shop/com/multiplexshop/domain/orders/controller/OrdersAPIController.java
+++ src/main/java/backend/shop/com/multiplexshop/domain/orders/controller/OrdersAPIController.java
@@ -1,9 +1,26 @@
 package backend.shop.com.multiplexshop.domain.orders.controller;
 
+import backend.shop.com.multiplexshop.domain.orders.dto.OrdersDTOs;
+import backend.shop.com.multiplexshop.domain.orders.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static backend.shop.com.multiplexshop.domain.orders.dto.OrdersDTOs.*;
 
 @RestController
 @RequiredArgsConstructor
 public class OrdersAPIController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/api/order")
+    public ResponseEntity<OrderResponseDTO> postOrder(@RequestBody OrderRequestDTO request){
+        OrderResponseDTO postOrderByRequest = orderService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(postOrderByRequest);
+    }
+
 }
---
+++ src/test/java/backend/shop/com/multiplexshop/domain/orders/controller/OrdersAPIControllerTest.java
@@ -0,0 +1,20 @@
+package backend.shop.com.multiplexshop.domain.orders.controller;
+
+import backend.shop.com.multiplexshop.domain.orders.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrdersAPIControllerTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    Moc
+
+}
